47c21a8f81ef2897fc3c2914d9083d39ab6785f2
Andrea Campolonghi <acampolonghi@gmail.com>
2016-05-15 19:27:02 +0200

    Learning more stuff

---
+++ web/controllers/user_controller.ex
@@ -0,0 +1,16 @@
+defmodule Rumbl.UserController do
+  @moduledoc false
+
+  use Rumbl.Web, :controller
+
+  def index(conn, _params) do
+    users = Repo.all(Rumbl.User)
+    render(conn, "index.html", users: users)
+  end
+
+  def show(conn, %{"id" => id}) do
+    user = Repo.get(Rumbl.User,id)
+    render(conn, "show.html", user: user)
+  end
+
+end
