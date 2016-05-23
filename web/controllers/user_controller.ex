542acca79c432c4cf59096ddf57a7d4b5a26ac6c
Andrea Campolonghi <acampolonghi@gmail.com>
2016-05-23 22:28:57 +0200

    More on changesets

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -19,4 +19,16 @@ defmodule Rumbl.UserController do
     render(conn, "new.html", changeset: changeset)
   end
 
+  def create(conn, %{"user" => user_params}) do
+    changeset = User.registration_changeset(%User{}, user_params)
+    case Repo.insert(changeset) do
+      { :ok, user } ->
+        conn
+        |> put_flash(:info, "#{user.name} created!")
+        |> redirect(to: user_path(conn, :index))
+      { :error, changeset } ->
+        render(conn, "new.html", changeset: changeset)
+    end
+  end
+
 end
--- web/router.ex
+++ web/router.ex
@@ -15,8 +15,6 @@ defmodule Rumbl.Router do
 
   scope "/", Rumbl do
     pipe_through :browser # Use the default browser stack
-    get "/users/", UserController, :index
-    get "/users/:id", UserController, :show
     resources "/users", UserController, only: [:create, :show, :new, :index]
     get "/", PageController, :index
   end
