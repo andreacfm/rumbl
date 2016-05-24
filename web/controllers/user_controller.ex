9602d42f09fa57b49d8a072264b982a075c9347a
Andrea Campolonghi <acampolonghi@gmail.com>
2016-05-24 22:40:45 +0200

    Making plugs

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -2,6 +2,7 @@ defmodule Rumbl.UserController do
   @moduledoc false
 
   use Rumbl.Web, :controller
+  plug :authenticate when action in [:index, :show]
   alias Rumbl.User
 
   def index(conn, _params) do
@@ -24,10 +25,22 @@ defmodule Rumbl.UserController do
     case Repo.insert(changeset) do
       { :ok, user } ->
         conn
+        |> Rumbl.Auth.login(user)
         |> put_flash(:info, "#{user.name} created!")
         |> redirect(to: user_path(conn, :index))
       { :error, changeset } ->
         render(conn, "new.html", changeset: changeset)
+    end
+  end
+
+  defp authenticate(conn, _opts) do
+    if conn.assigns.current_user do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You must be logged in!")
+      |> redirect(to: page_path(conn, :index))
+      |> halt()
     end
   end
 
--- web/router.ex
+++ web/router.ex
@@ -7,6 +7,7 @@ defmodule Rumbl.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug Rumbl.Auth, repo: Rumbl.Repo
   end
 
   pipeline :api do
@@ -15,6 +16,7 @@ defmodule Rumbl.Router do
 
   scope "/", Rumbl do
     pipe_through :browser # Use the default browser stack
+    resources("/sessions", SessionController, only: [:new, :create, :delete])
     resources "/users", UserController, only: [:create, :show, :new, :index]
     get "/", PageController, :index
   end
