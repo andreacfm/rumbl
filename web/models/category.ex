c6265cb620a3bc086063d990d76f3b163304625d
Andrea Campolonghi <acampolonghi@gmail.com>
2016-06-05 12:45:07 +0200

    Ecto

--- web/models/category.ex
+++ web/models/category.ex
@@ -1,7 +1,7 @@
 defmodule Rumbl.Category do
   use Rumbl.Web, :model
 
-  schema "catgeories" do
+  schema "categories" do
     field :name, :string
 
     timestamps
@@ -20,4 +20,13 @@ defmodule Rumbl.Category do
     model
     |> cast(params, @required_fields, @optional_fields)
   end
+
+  def alphabetical(query) do
+    from c in query, order_by: c.name
+  end
+
+  def names_and_ids(query) do
+    from c in query, select: {c.name, c.id}
+  end
+
 end
